c37891a11006f70aa1559f9caf083f453a27bda6
amiof <arm131313@gmail.com>
2023-06-06 11:59:14 +0330

    add navbar to neotree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -78,6 +78,7 @@ return {
           folder_closed = "",
           folder_open = "",
           folder_empty = "ﰊ",
+          folder_empty_open = "󰜌",
           -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
           -- then these will never be used.
           default = "*",
@@ -106,6 +107,38 @@ return {
             staged    = "",
             conflict  = "",
           }
+        },
+        document_symbols = {
+          kinds = {
+            File = { icon = "󰈙", hl = "Tag" },
+            Namespace = { icon = "󰌗", hl = "Include" },
+            Package = { icon = "󰏖", hl = "Label" },
+            Class = { icon = "󰌗", hl = "Include" },
+            Property = { icon = "󰆧", hl = "@property" },
+            Enum = { icon = "󰒻", hl = "@number" },
+            Function = { icon = "󰊕", hl = "Function" },
+            String = { icon = "󰀬", hl = "String" },
+            Number = { icon = "󰎠", hl = "Number" },
+            Array = { icon = "󰅪", hl = "Type" },
+            Object = { icon = "󰅩", hl = "Type" },
+            Key = { icon = "󰌋", hl = "" },
+            Struct = { icon = "󰌗", hl = "Type" },
+            Operator = { icon = "󰆕", hl = "Operator" },
+            TypeParameter = { icon = "󰊄", hl = "Type" },
+            StaticMethod = { icon = '󰠄 ', hl = 'Function' },
+          }
+        },
+      },
+      source_selector = {
+        winbar = true,
+        statusline = true,
+        buffers = true,
+        document_symbols = true,
+        sources = {
+          { source = "filesystem",       display_name = " 󰉓 Files " },
+          { source = "git_status",       display_name = " 󰊢 Git " },
+          { source = "buffers",          display_name = " 󰈙 Buffers" },
+          { source = "document_symbols", display_name = " 󰈙 symbols" },
         },
       },
       window = {
