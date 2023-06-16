bd1108d00786fe4c0c622b50c160dbe1b774d064
amiof <arm131313@gmail.com>
2023-06-16 10:42:04 +0330

    format bufferline

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,38 +1,37 @@
 return {
-  {
-    'akinsho/bufferline.nvim',
-    version = "v3.*",
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function()
-      vim.opt.termguicolors = true
-      require("bufferline").setup {
-        options = {
-          buffer_close_icon = ' ', --'',
-          modified_icon = '●',
-          close_icon = '',
-          left_trunc_marker = '',
-          right_trunc_marker = '',
-          hover = {
-            enabled = true,
-            delay = 200,
-            reveal = { 'close' }
-          },
-          indicator = {
-            icon = '▎',      -- this should be omitted if indicator style is not 'icon'
-            style = "icon"     --'icon' | 'underline' | 'none',
-          },
-          offsets = {
-            {
-              filetype = "NvimTree",
-              text = "File Explorer",
-              separator = true,  -- use a "true" to enable the def
-              text_aligin = "left",
+    {
+        'akinsho/bufferline.nvim',
+        version = "v3.*",
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        config = function()
+            vim.opt.termguicolors = true
+            require("bufferline").setup {
+                options = {
+                    buffer_close_icon = ' ', --'',
+                    modified_icon = '●',
+                    close_icon = '',
+                    left_trunc_marker = '',
+                    right_trunc_marker = '',
+                    hover = {
+                        enabled = true,
+                        delay = 200,
+                        reveal = { 'close' }
+                    },
+                    indicator = {
+                        icon = '▎',  -- this should be omitted if indicator style is not 'icon'
+                        style = "icon" --'icon' | 'underline' | 'none',
+                    },
+                    offsets = {
+                        {
+                            filetype = "NvimTree",
+                            text = "File Explorer",
+                            separator = true, -- use a "true" to enable the def
+                            text_aligin = "left",
+                        }
+                    },
+                    separator_style = "slant", --thin or slope or thick or slant padded_slant
+                }
             }
-          },
-          separator_style = "slant", --thin or slope or thick or slant padded_slant
-        }
-      }
-    end,
-  }
+        end,
+    }
 }
-
