cf99c79a805515f7a630993cbe271bc97bb1b94a
amiof <arm131313@gmail.com>
2023-06-20 15:10:26 +0330

    change name and add jest neotest config

---
+++ lua/custom/plugins/rust-pde-neotest.lua
@@ -0,0 +1,33 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "rouge8/neotest-rust",
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-neotest/neotest-vim-test",
+    "vim-test/vim-test",
+    'haydenmeade/neotest-jest',
+  },
+  config = function()
+    -- vim.list_extend(opts.adapters, {
+    --   require "neotest-rust",
+    -- })
+    require("neotest").setup({
+      adapters = {
+        require("neotest-rust") {
+          args = { "--no-capture" },
+          dap_adapter = "lldb",
+        },
+        require('neotest-jest')({
+          jestCommand = "npm test --",
+          jestConfigFile = "custom.jest.config.ts",
+          env = { CI = true },
+          cwd = function()
+            return vim.fn.getcwd()
+          end,
+        }),
+      }
+    })
+  end,
+}
