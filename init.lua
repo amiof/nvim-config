5bb1e299a7fd733a40f85cc81b3c2e9292c37a42
amiof <arm131313@gmail.com>
2023-07-10 07:37:44 +0330

    fix diagnostic open float key

--- init.lua
+++ init.lua
@@ -375,7 +375,7 @@ require('nvim-treesitter.configs').setup {
 vim.keymap.set('n', '<leader>lj', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
 vim.keymap.set('n', '<leader>lk', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
 vim.keymap.set('n', '<leader>lq', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set('n', '<leader>le', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set('n', '<leader>ld', ":Telescope diagnostics<cr>", { silent = true })
 
 -- LSP settings.
