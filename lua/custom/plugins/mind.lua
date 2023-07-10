1b138fb61d11942cc7159fc0bc755fd87e65ee42
amiof <arm131313@gmail.com>
2023-07-10 07:36:05 +0330

    change mind config

--- lua/custom/plugins/mind.lua
+++ lua/custom/plugins/mind.lua
@@ -3,6 +3,34 @@ return {
     branch = 'v2.2',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-        require 'mind'.setup()
+        require 'mind'.setup({
+            persistence = {
+                state_path = "~/mind/mind.json",
+                data_dir = "~/mind/data"
+            },
+            edit = {
+                data_header = "# %s", },
+            ui = {
+                width = 40,
+                -- icon_preset = {
+                --     { " ", "Sub-project" },
+                --     { " ", "Journal, newspaper, weekly and daily news" },
+                --     { " ", "For when you have an idea" },
+                --     { " ", "Note taking?" },
+                --     { "陼",  "Task management" },
+                --     { " ", "Uncheck, empty square or backlog" },
+                --     { " ", "Full square or on-going" },
+                --     { " ", "Check or done" },
+                --     { " ", "Trash bin, deleted, cancelled, etc." },
+                --     { " ", "GitHub" },
+                --     { " ", "Monitoring" },
+                --     { " ", "Internet, Earth, everyone!" },
+                --     { " ", "Frozen, on-hold" },
+                -- },
+                highlight = {
+                    node_root = 'Number',
+                }
+            },
+        })
     end
 }
