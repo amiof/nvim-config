81031087ad0813cc8c5a176634657b61cb528c46
amiof <arm131313@gmail.com>
2023-07-14 00:03:35 +0330

    add new repo rainbow for show color in ()

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -17,36 +17,40 @@ return {
   build = ":TSUpdate",
 
   config = function()
-  local configs = require "nvim-treesitter.configs"
+    local configs = require "nvim-treesitter.configs"
 
-  configs.setup {
+    configs.setup {
 
-  ensure_installed = "all",
+      ensure_installed = "all",
 
-  ignore_install = {"haskell","haskell_persistent"}, -- List of parsers to ignore installing
-    highlight = {
-      enable = true, -- false will disable the whole extension
-      disable = {"yaml"}, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
-    },
-    autopairs = {
-      enable = true,
-    },
-    indent = {
-      enable = false,
-      disable = {}
-    },
+      ignore_install = { "haskell", "haskell_persistent" }, -- List of parsers to ignore installing
+      highlight = {
+        enable = true,                                      -- false will disable the whole extension
+        disable = { "yaml" },                               -- list of language that will be disabled
+        additional_vim_regex_highlighting = true,
+      },
+      autopairs = {
+        enable = true,
+      },
+      indent = {
+        enable = false,
+        disable = {}
+      },
 
       rainbow = {
         enable = true,
-        extended_mode = false,
+        extended_mode = true,
+        disable = { 'cpp' },
+        -- Which query to use for finding delimiters
+        query = 'rainbow-parens',
+        -- Highlight the entire buffer all at once
+        strategy = require('ts-rainbow').strategy.global,
       },
       autotag = {
         enable = true,
       },
 
-    sync_install = false,
-  }
+      sync_install = false,
+    }
   end
 }
-
