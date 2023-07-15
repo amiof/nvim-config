a03f9a790b9a300a93dde053afee04f70745c80e
amiof <arm131313@gmail.com>
2023-07-15 21:26:47 +0330

    neotree version 3 migration

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
@@ -223,7 +224,12 @@ return {
             --".null-ls_*",
           },
         },
-        follow_current_file = false,            -- This will find and focus the file in the active buffer every
+        -- follow_current_file = true,            --for version2 This will find and focus the file in the active buffer every
+          follow_current_file = {
+            enabled = false, -- This will find and focus the file in the active buffer every time
+            --               -- the current file is changed while the tree is open.
+            leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+          },
         -- time the current file is changed while the tree is open.
         group_empty_dirs = false,               -- when true, empty folders will be grouped together
         hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
@@ -257,7 +263,12 @@ return {
         }
       },
       buffers = {
-        follow_current_file = true, -- This will find and focus the file in the active buffer every
+        -- follow_current_file = true, -- This will find and focus the file in the active buffer every
+           follow_current_file = {
+            enabled = true, -- This will find and focus the file in the active buffer every time
+            --              -- the current file is changed while the tree is open.
+            leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+          },
         -- time the current file is changed while the tree is open.
         group_empty_dirs = true,    -- when true, empty folders will be grouped together
         show_unloaded = true,
