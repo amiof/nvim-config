be992ed8ed28edf9d7143c35af62fa5779d529df
amiof <arm131313@gmail.com>
2023-09-24 09:42:48 +0330

    makge better leaderf

--- lua/Config/keybinding.lua
+++ lua/Config/keybinding.lua
@@ -126,10 +126,10 @@ local keymap = vim.keymap
 keymap.amend = require('keymap-amend')
 local map = require('fold-preview').mapping
 
-keymap.amend('n', 'h',  map.show_close_preview_open_fold)
-keymap.amend('n', 'l',  map.close_preview_open_fold)
-keymap.amend('n', 'k',  map.close_preview_without_defer)
-keymap.amend('n', 'j',  map.close_preview_without_defer)
+keymap.amend('n', 'h', map.show_close_preview_open_fold)
+keymap.amend('n', 'l', map.close_preview_open_fold)
+keymap.amend('n', 'k', map.close_preview_without_defer)
+keymap.amend('n', 'j', map.close_preview_without_defer)
 keymap.amend('n', 'zo', map.close_preview)
 keymap.amend('n', 'zO', map.close_preview)
 keymap.amend('n', 'zc', map.close_preview_without_defer)
--- lua/custom/plugins/leaderf.lua
+++ lua/custom/plugins/leaderf.lua
@@ -2,5 +2,8 @@ return {
     "Yggdroot/LeaderF",
     config = function()
         vim.g.Lf_WindowPosition = "popup"
+        vim.cmd("let g:Lf_StlSeparator = { 'right': '', 'left': '' }")
+        vim.cmd('let g:Lf_PopupBorders = ["═","║","═","║","╔","╗","╝","╚"]')
+        -- vim.cmd("let g:Lf_StlColorscheme = 'tokyonight'")
     end
 }
