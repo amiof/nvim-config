b125048d0b5e843a79063db05c5cb0229df25de3
amiof <arm131313@gmail.com>
2023-09-24 14:30:10 +0330

    add some thing for leaderf

--- lua/custom/plugins/leaderf.lua
+++ lua/custom/plugins/leaderf.lua
@@ -4,6 +4,9 @@ return {
         vim.g.Lf_WindowPosition = "popup"
         vim.cmd("let g:Lf_StlSeparator = { 'right': '', 'left': '' }")
         vim.cmd('let g:Lf_PopupBorders = ["═","║","═","║","╔","╗","╝","╚"]')
-        -- vim.cmd("let g:Lf_StlColorscheme = 'tokyonight'")
+        vim.cmd("let g:Lf_PopupColorscheme = 'onedark'")
+        vim.cmd("let g:Lf_WorkingDirectoryMode = 'a'")
+        vim.cmd("let g:Lf_UseMemoryCache = 0")
+        -- vim.cmd("let g:Lf_PopupPreviewPosition='bottom'")
     end
 }
