6305560ac7de2212b79885725d77f18e7b280900
amiof <arm131313@gmail.com>
2023-10-19 08:51:23 +0330

    change config for indent-blankline for v3 and mini-indent

--- lua/custom/plugins/indent-guides.lua
+++ lua/custom/plugins/indent-guides.lua
@@ -8,58 +8,102 @@
 vim.opt.list = true
 -- vim.opt.listchars:append "space:⋅"
 -- vim.opt.listchars:append "eol:↴"
+-- return {
+--   {
+--     "lukas-reineke/indent-blankline.nvim",
+--     event = {
+--       "BufReadPost", "BufNewFile"
+--     },
+--     opts = {
+--       -- char = "│",
+--       -- char = '┊',
+--       char_list = { "", "┊", "┆", "¦", "|", "¦", "┆", "┊", "" },
+--       char_highlight_list = {
+--         "IndentBlanklineIndent1",
+--         "IndentBlanklineIndent2",
+--         "IndentBlanklineIndent3",
+--         "IndentBlanklineIndent4",
+--         "IndentBlanklineIndent5",
+--         "IndentBlanklineIndent6",
+--       },
+--       filetype_exclude = {
+--         "help", "alpha", "dashboard", "NvimTree", "Trouble", "lazy"
+--       },
+--       show_trailing_blankline_indent = false,
+--       show_current_context = false,
+--     },
+--   },
+--
+--   {
+--     "echasnovski/mini.indentscope",
+--     version = false,
+--     event = {
+--       "BufReadPre", "BufNewFile"
+--     },
+--     opts = {
+--       symbol = "│",
+--       -- symbol = '┊',
+--
+--       options = {
+--         try_as_border = true
+--       },
+--     },
+--     init = function()
+--       vim.api.nvim_create_autocmd("FileType", {
+--         pattern = {
+--           "help", "alpha", "dashboard", "NvimTree", "Trouble", "lazy", "mason"
+--         },
+--         callback = function()
+--           vim.b.miniindentscope_disable = true
+--         end,
+--       })
+--     end,
+--     config = function(_, opts)
+--       require("mini.indentscope").setup(opts)
+--     end,
+--   },
+-- }
+--
+--
+--
+--config for v3  indent0-blankline
 return {
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    event = {
-      "BufReadPost", "BufNewFile"
-    },
-    opts = {
-      -- char = "│",
-      -- char = '┊',
-      char_list = { "", "┊", "┆", "¦", "|", "¦", "┆", "┊", "" },
-      char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
-        "IndentBlanklineIndent5",
-        "IndentBlanklineIndent6",
-      },
-      filetype_exclude = {
-        "help", "alpha", "dashboard", "NvimTree", "Trouble", "lazy"
-      },
-      show_trailing_blankline_indent = false,
-      show_current_context = false,
-    },
-  },
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {},
 
-  {
-    "echasnovski/mini.indentscope",
-    version = false,
-    event = {
-      "BufReadPre", "BufNewFile"
-    },
-    opts = {
-      symbol = "│",
-      -- symbol = '┊',
-
-      options = {
-        try_as_border = true
+  config = function()
+    local highlight = {
+      "RainbowRed",
+      "RainbowYellow",
+      "RainbowBlue",
+      "RainbowOrange",
+      "RainbowGreen",
+      "RainbowViolet",
+      "RainbowCyan",
+      "white"
+    }
+    local hooks = require "ibl.hooks"
+    -- create the highlight groups in the highlight setup hook, so they are reset
+    -- every time the colorscheme changes
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+      vim.api.nvim_set_hl(0, "white", { fg = "#ffffff" })
+    end)
+    require("ibl").setup {
+      indent = { highlight = highlight, char = { "", "┊", "┆", "¦", "|", "¦", "┆", "┊", "" },
       },
-    },
-    init = function()
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = {
-          "help", "alpha", "dashboard", "NvimTree", "Trouble", "lazy", "mason"
-        },
-        callback = function()
-          vim.b.miniindentscope_disable = true
-        end,
-      })
-    end,
-    config = function(_, opts)
-      require("mini.indentscope").setup(opts)
-    end,
-  },
+      whitespace = {
+        highlight = highlight,
+        remove_blankline_trail = false,
+      },
+      scope = { enabled = false },
+    }
+  end,
 }
